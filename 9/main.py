8a3404601c9c235b350aea78a342bda654beba52
Martin Váňa <hallowedbythyname@seznam.cz>
2021-12-09 22:32:50 +0100

    9. problem - iterative solution

--- 9/main.py
+++ 9/main.py
@@ -38,7 +38,7 @@ with open(FILE) as f:
                 sum_of_low_points += 1 + int(h)
     print(sum_of_low_points)
 
-# 2. part - find basins
+# 2. part - find basins (recursive)
 from functools import reduce
 
 FLOODED = 'x'
@@ -80,6 +80,75 @@ with open(FILE) as f:
             # Start new flooding
             if h not in [FLOODED, MAX_HEIGHT]:
                 basin_sizes.append(flood_fill(A, x, y))
+
+    basin_sizes.sort()
+
+    # Multiply 3 bigest basins
+    print(reduce((lambda x, y: x * y), basin_sizes[-3:]))
+
+# 2. part - find basins (iterative)
+from collections import deque
+
+with open(FILE) as f:
+    A = []
+    for line in f.readlines():
+        A.append(list(line.strip()))
+
+    x_dim = len(A[0])
+    y_dim = len(A)
+
+    basin_sizes = []
+
+    def flood_fill(A, x, y):
+        flooded = 1
+
+        A[y][x] = FLOODED
+
+        for d in DIRECTIONS:
+            new_x = x + d[0]
+            new_y = y + d[1]
+
+            if new_x < 0 or new_x >= x_dim or new_y < 0 or new_y >= y_dim:
+                # outside of area
+                continue
+
+            # Don't flood what is already flooded and max height
+            if A[new_y][new_x] not in [FLOODED, MAX_HEIGHT]:
+                flooded += flood_fill(A, new_x, new_y)
+
+        return flooded
+
+    for y in range(y_dim):
+        for x in range(x_dim):
+            # Start new flooding
+            if A[y][x] not in [FLOODED, MAX_HEIGHT]:
+                flooded = 0
+                Q = deque()
+                Q.append((x, y))
+                while Q:
+                    qx, qy = Q.pop()
+
+                    if A[qy][qx] in [FLOODED, MAX_HEIGHT]:
+                        continue
+
+                    A[qy][qx] = FLOODED
+                    flooded += 1
+
+                    for d in DIRECTIONS:
+                        new_x = qx + d[0]
+                        new_y = qy + d[1]
+
+                        if new_x < 0 or new_x >= x_dim or new_y < 0 or new_y >= y_dim:
+                            # outside of area
+                            continue
+
+                        # Don't flood what is already flooded and max height
+                        if A[new_y][new_x] in [FLOODED, MAX_HEIGHT]:
+                            continue
+
+                        Q.append((new_x, new_y))
+
+                basin_sizes.append(flooded)
 
     basin_sizes.sort()
 
